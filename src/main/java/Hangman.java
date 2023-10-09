f887dfe057596a50bd976c0ee49ce2c0ca6a6d02
DevMattyJS <matej.sliacky@protonmail.com>
2023-10-09 09:45:17 +0200

    Added feature, that words are randomly pick up from a file

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,5 +1,7 @@
-import java.util.HashSet;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Hangman {
 
@@ -9,14 +11,15 @@ public class Hangman {
     private HashSet<Character> guessedLetters;
     private HangmanParts hangmanStage;
 
-    public Hangman(String secretWord) {
-        this.secretWord = secretWord;
+    public Hangman() {
+        this.secretWord = takeRandomWordFromFile("nouns.txt");
         this.attempts = 7;
         this.guessedWord = new StringBuilder("_".repeat(secretWord.length()));
         this.guessedLetters = new HashSet<>();
         this.hangmanStage = HangmanParts.START;
     }
 
+    // A method, that contains game loop
     public void play() {
         Scanner scanner = new Scanner(System.in);
         int remainingAttempts = attempts;
@@ -24,14 +27,17 @@ public class Hangman {
         while (remainingAttempts > 0) {
             System.out.printf("Attempts remaining: %d\n", remainingAttempts);
             System.out.printf("Secret word: %s\n", guessedWord);
-            System.out.println("Enter a letter: ");
+            System.out.print("Enter a letter: ");
+
             char guessedLetter = scanner.next().charAt(0);
 
+            // Checks if the letter was entered by a player in this game
             if (guessedLetters.contains(guessedLetter)) {
                 System.out.println("Invalid attempt. You already guessed this letter before.");
                 continue;
             }
 
+            // Each letter, that was already entered by a player is inserted to guessedLetters set
             guessedLetters.add(guessedLetter);
 
             // If the letter player guessed is in the secret word => replace all "_" at correct positions by that letter
@@ -47,31 +53,35 @@ public class Hangman {
                     break;
                 }
             } else {
+                // If guessed letter is not in a secret word, a message and next hangman stage is printed, remainingAttempts are deducted
                 System.out.printf("I'm sorry, letter '%c' is not contained in a secret word.\n", guessedLetter);
                 hangmanStage = hangmanStage.getNextStage();
                 System.out.printf("Hangman stage:\n%s", hangmanStage.getArt());
                 remainingAttempts--;
             }
 
+            // If there are no more remaining attempts left => game over
             if (remainingAttempts == 0) {
                 System.out.println("Game over! You are out of attempts.");
+                System.out.printf("The secret word was: %s\n", secretWord);
             }
         }
     }
 
-    public String getSecretWord() {
-        return secretWord;
-    }
+    // Picks a random word from a file (words must be each on a separate line)
+    private String takeRandomWordFromFile(String fileName) {
+        List<String> words = new ArrayList<>();
+        Random random = new Random();
+        try(BufferedReader bf = new BufferedReader(new FileReader(fileName))) {
+            String word = bf.readLine();
+            while (word != null) {
+                words.add(word);
+                word = bf.readLine();
+            }
+        } catch (IOException e) {
+            System.out.printf("I/O error: %s\n", e.getMessage());
+        }
 
-    public int getAttempts() {
-        return attempts;
-    }
-
-    public StringBuilder getGuessedWord() {
-        return guessedWord;
-    }
-
-    public HashSet<Character> getGuessedLetters() {
-        return guessedLetters;
+        return words.get(random.nextInt(words.size()));
     }
 }
--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -2,7 +2,7 @@ public class HangmanGame {
 
     public static void main(String[] args) {
 
-        Hangman hangman = new Hangman("apple");
+        Hangman hangman = new Hangman();
 //        System.out.println(hangman.getGuessedWord());
         hangman.play();
     }
